f4f904ed8899752c2c26ac29c00133be18255b74
premd132 <premd132@gmail.com>
2026-01-05 02:44:37 +0530

    Update pattern_engine_basic.js

--- pattern_engine_basic.js
+++ pattern_engine_basic.js
@@ -1,30 +1,22 @@
 function basicEngine(data,r,c){
-  let singlesCount={};
-  let points=[];
-
+  let singles={}, points=[];
   for(let i=r-1;i>=0 && i>=r-10;i--){
-    for(let j=0;j<6;j++){
-      let v=data[i][j];
-      if(v){
-        v.split("").forEach(d=>{
-          singlesCount[d]=(singlesCount[d]||0)+1;
-        });
-        points.push({r:i,c:j});
-      }
-    }
+    let v=data[i][c];
+    if(!v) continue;
+    v.split("").forEach(d=>{
+      singles[d]=(singles[d]||0)+1;
+    });
+    points.push({r:i,c});
   }
 
-  let singles = Object.entries(singlesCount)
+  let top = Object.entries(singles)
     .sort((a,b)=>b[1]-a[1])
-    .slice(0,3)
-    .map(x=>x[0]);
+    .slice(0,3).map(x=>x[0]);
 
   let jodi=[];
-  singles.forEach(a=>{
-    singles.forEach(b=>{
-      if(jodi.length<8) jodi.push(a+b);
-    });
-  });
+  top.forEach(a=>top.forEach(b=>{
+    if(jodi.length<6) jodi.push(a+b);
+  }));
 
-  return {points,singles,jodi};
+  return {points, singles:top, jodi};
 }
