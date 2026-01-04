ddd26ae12f2cac59df7858fec1a592add7c8f49b
premd132 <premd132@gmail.com>
2026-01-05 02:46:53 +0530

    Update pattern_engine_hp80.js

--- pattern_engine_hp80.js
+++ pattern_engine_hp80.js
@@ -1,16 +1,3 @@
 function hp80Engine(data,r,c){
-  let singles=["2","4","8"];
-  let points=[];
-  for(let i=r-1;i>=0 && i>=r-10;i--){
-    points.push({r:i,c});
-  }
-
-  let jodi=[];
-  singles.forEach(a=>{
-    singles.forEach(b=>{
-      if(jodi.length<8) jodi.push(a+b);
-    });
-  });
-
-  return {points,singles,jodi};
+  return basicEngine(data,r,c);
 }
