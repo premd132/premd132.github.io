824f8544d5eed0fd88f85f4fb1e3ac5ff59cc048
premd132 <premd132@gmail.com>
2026-01-05 02:45:31 +0530

    Update pattern_engine_family.js

--- pattern_engine_family.js
+++ pattern_engine_family.js
@@ -1,27 +1,3 @@
 function familyEngine(data,r,c){
-  const fam = n=>[
-    n, n.split("").reverse().join("")
-  ];
-
-  let singles=[];
-  let points=[];
-
-  for(let i=r-1;i>=0 && i>=r-10;i--){
-    let v=data[i][c];
-    if(v){
-      singles.push(...v.split(""));
-      points.push({r:i,c});
-    }
-  }
-
-  singles=[...new Set(singles)].slice(0,3);
-
-  let jodi=[];
-  singles.forEach(s=>{
-    fam(s+s).forEach(j=>{
-      if(jodi.length<8) jodi.push(j);
-    });
-  });
-
-  return {points,singles,jodi};
+  return basicEngine(data,r,c);
 }
