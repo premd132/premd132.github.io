82a61fc83bcc33dba1e5506836e9b9e69eb1b659
premd132 <premd132@gmail.com>
2026-01-05 01:57:00 +0530

    Update pattern_engine_hp80.js

--- pattern_engine_hp80.js
+++ pattern_engine_hp80.js
@@ -1,24 +1,16 @@
-function hp80Engine(data, r, c) {
-  let count = {};
-  let points = [];
-
-  for (let col = c - 1; col <= c + 1; col++) {
-    for (let i = r - 1; i >= Math.max(0, r - 12); i--) {
-      if (!data[i] || !data[i][col]) continue;
-
-      const v = data[i][col];
-      count[v] = (count[v] || 0) + 1;
-      points.push({ r: i, c: col });
-    }
+function hp80Engine(data,r,c){
+  let singles=["2","4","8"];
+  let points=[];
+  for(let i=r-1;i>=0 && i>=r-10;i--){
+    points.push({r:i,c});
   }
 
-  const strong = Object.entries(count)
-    .filter(x => x[1] >= 2)
-    .sort((a, b) => b[1] - a[1])
-    .slice(0, 3);
+  let jodi=[];
+  singles.forEach(a=>{
+    singles.forEach(b=>{
+      if(jodi.length<8) jodi.push(a+b);
+    });
+  });
 
-  const singles = strong.map(x => x[0][0]);
-  const jodi = strong.map(x => x[0]);
-
-  return { singles, jodi, points };
+  return {points,singles,jodi};
 }
