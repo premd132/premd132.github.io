330f29d505d47cdf1aa7f208ff77fbf75549a74a
premd132 <premd132@gmail.com>
2026-01-04 23:52:06 +0530

    Update pattern_engine_basic.js

--- pattern_engine_basic.js
+++ pattern_engine_basic.js
@@ -1,24 +1,21 @@
-function basicEngine(data,r,c){
-  let freq={};
-  let pts=[];
+function basicEngine(data, r, c) {
+  let freq = {};
+  let points = [];
 
-  for(let i=r-1;i>=0 && i>=r-10;i--){
-    let v=data[i][c];
-    if(!v) continue;
-    pts.push({r:i,c});
-    v.split("").forEach(d=>{
-      freq[d]=(freq[d]||0)+1;
-    });
+  for (let i = r - 1; i >= Math.max(0, r - 10); i--) {
+    const v = data[i][c];
+    if (!v) continue;
+
+    freq[v] = (freq[v] || 0) + 1;
+    points.push({ r: i, c });
   }
 
-  let singles = Object.entries(freq)
-    .sort((a,b)=>b[1]-a[1])
-    .slice(0,3)
-    .map(x=>x[0]);
+  const sorted = Object.entries(freq)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3);
 
-  return{
-    points:pts,
-    html:`<b>Basic Pattern</b><br>
-    Strong Singles: ${singles.join(", ")}`
-  }
+  const singles = sorted.map(s => s[0]);
+  const jodi = singles.map(a => a + a);
+
+  return { singles, jodi, points };
 }
