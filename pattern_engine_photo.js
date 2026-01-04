9733e0022ba1d1979c7035d20089482810442fb4
premd132 <premd132@gmail.com>
2026-01-05 02:46:16 +0530

    Update pattern_engine_photo.js

--- pattern_engine_photo.js
+++ pattern_engine_photo.js
@@ -1,16 +1,3 @@
 function photoEngine(data,r,c){
-  let singles=["0","5","9"];
-  let points=[];
-  for(let i=r-1;i>=0 && i>=r-10;i--){
-    points.push({r:i,c});
-  }
-
-  let jodi=[];
-  singles.forEach(a=>{
-    singles.forEach(b=>{
-      if(jodi.length<8) jodi.push(a+b);
-    });
-  });
-
-  return {points,singles,jodi};
+  return basicEngine(data,r,c);
 }
